2c912cccad432954288c01de099a424abc698501
mohi14 <mohiuddinkhan1409@gmail.com>
2022-10-26 17:12:13 +0600

    fake data created for categories

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-/* 
-creating server for my client side website
-*/
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('API Is Running')
+});
+
+app.listen(port, () => {
+    console.log('Akadimia Server running on port', port)
+})
