167e8dd4da3cd429f11db62482e26fce1a13bf98
mohi14 <mohiuddinkhan1409@gmail.com>
2022-10-27 02:31:15 +0600

    API created for a particular course

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ app.get('/category/:id', (req, res) => {
     res.send(category_courses);
 });
 
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(c => c._id === id);
+    res.send(selectedCourse);
+})
+
 app.get('/courses', (req, res) => {
     res.send(courses)
 });
