c59d0120fd991a2eb77f7d712553d6f0ff23d3b5
mohi14 <mohiuddinkhan1409@gmail.com>
2022-10-26 18:48:20 +0600

    API created for course-category

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./data/categories.json');
+
 app.get('/', (req, res) => {
     res.send('API Is Running')
 });
+
+app.get('/course-category', (req, res) => {
+    res.send(categories)
+})
 
 app.listen(port, () => {
     console.log('Akadimia Server running on port', port)
