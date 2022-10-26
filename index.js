d486aa283910fa5095c0db5733b1536024ca9f38
mohi14 <mohiuddinkhan1409@gmail.com>
2022-10-26 19:14:53 +0600

    API for categories courses added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('API Is Running')
@@ -13,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/course-category', (req, res) => {
     res.send(categories)
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_courses = courses.filter(c => c.category_id === id);
+    res.send(category_courses);
 })
 
 app.listen(port, () => {
